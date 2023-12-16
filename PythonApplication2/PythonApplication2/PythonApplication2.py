9e8d099fdb6869614fe01fd503016c3a564946c5
fady hany <fadyhany.17.costa@gmail.com>
2023-12-16 14:51:26 +0200

    'first'

---
+++ PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -0,0 +1,3 @@
+myAwesomeList = ["One", "Two", "One", 1, 100.5, True]
+print(myAwesomeList)  # Whole List
+
---
+++ PythonApplication4/PythonApplication4/PythonApplication4.py
@@ -0,0 +1,11 @@
+name = "fady"
+age = 19
+rank = 10
+print("My Name is: " + name)
+print("My Name is: %s" % "Osama")
+print("My Name is: %s" % name)
+print("My Name is: %s and My Age is: %d" % (name, age))
+print("My Name is: %s and My Age is: %d and my rank is : %f " % (name, age,rank))
+# %s => String
+# %d => Number
+# %f => Float
